90b119fba6781b56dc245fae17184c1e7b6a20a0
gabriela conde <gabiconde97@gmail.com>
2019-08-29 21:06:08 -0300

    add foreignKey

--- src/database/migrations/20190829011005-add-avatar-field-to-user.js
+++ src/database/migrations/20190829011005-add-avatar-field-to-user.js
@@ -2,14 +2,14 @@ module.exports = {
   up: (queryInterface, Sequelize) => {
     return queryInterface.addColumn('users', 'avatar_id', {
       type: Sequelize.INTEGER,
-      reference: { model: 'files', key: 'id' },
+      references: { model: 'files', key: 'id' },
       onUpdate: 'CASCADE',
       onDelete: 'SET NULL',
       allowNull: true,
     });
   },
 
-  down: (queryInterface, Sequelize) => {
+  down: queryInterface => {
     return queryInterface.removeColumn('users', 'avatar_id');
   },
 };
