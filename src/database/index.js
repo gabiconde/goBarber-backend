78238e99fd72a3f69a9718a080a01ace08939f90
gabriela conde <gabiconde97@gmail.com>
2019-08-31 11:55:35 -0300

    Mongo connection

--- src/database/index.js
+++ src/database/index.js
@@ -1,6 +1,7 @@
 // conexão com db
 // import models
 import Sequelize from 'sequelize';
+import mongoose from 'mongoose';
 
 import User from '../app/models/User';
 import File from '../app/models/File';
@@ -12,6 +13,7 @@ const models = [User, File, Appointment];
 class Database {
   constructor() {
     this.init();
+    this.mongo();
   }
 
   init() {
@@ -24,6 +26,16 @@ class Database {
       model => model.associate && model.associate(this.connection.models)
     );
   }
+
+  mongo() {
+    this.mongoConnection = mongoose.connect(
+      'mongodb://localhost:27017/gobarber',
+      { useNewUrlParser: true, useFindAndModify: true }
+    );
+  }
 }
+
+// { useNewUrlParser: true, useFindAndModify: true }
+// Usar o novo formato da url, Fazer alterações no banco
 
 export default new Database();
