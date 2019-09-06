273c765dbe80ff25d1ee86ed3494ee9d8bb3473d
gabiconde <gabiconde97@gmail.com>
2019-09-06 17:01:10 -0300

    Nodemailer and template config

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '1978693671155b',
+    pass: '53f4fca13a6da9',
+  },
+  default: {
+    from: 'Equipe Gobarber <noreply@gobarber.com>',
+  },
+};
---
+++ src/lib/Mail.js
@@ -0,0 +1,46 @@
+import nodemailer from 'nodemailer';
+import { resolve } from 'path';
+import exphbs from 'express-handlebars';
+import nodemailerhbs from 'nodemailer-express-handlebars';
+import configMail from '../config/mail';
+
+class Mail {
+  constructor() {
+    const { auth, port, host, secure } = configMail;
+    this.transporter = nodemailer.createTransport({
+      port,
+      host,
+      secure,
+      auth: auth.user ? auth : null,
+    });
+
+    this.configureTemplates();
+  }
+
+  configureTemplates() {
+    const viewPath = resolve(__dirname, '..', 'app', 'views', 'emails');
+
+    this.transporter.use(
+      'compile',
+      nodemailerhbs({
+        viewEngine: exphbs.create({
+          layoutsDir: resolve(viewPath, 'layouts'),
+          partialsDir: resolve(viewPath, 'partials'),
+          defaultLayout: 'default',
+          extname: '.hbs',
+        }),
+        viewPath,
+        extName: '.hbs',
+      })
+    );
+  }
+
+  sendMail(message) {
+    return this.transporter.sendMail({
+      ...configMail.default,
+      ...message,
+    });
+  }
+}
+
+export default new Mail();
