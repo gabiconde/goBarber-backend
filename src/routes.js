a3cccf3dd9a0cfa242d90d1d016b1e8ade94e0ea
gabiconde <gabiconde97@gmail.com>
2019-09-07 23:53:33 -0300

    Listing available hours by provider

--- src/routes.js
+++ src/routes.js
@@ -10,7 +10,7 @@ import ProviderController from './app/controllers/ProviderController';
 import AppointmentController from './app/controllers/AppointmentController';
 import ScheduleController from './app/controllers/ScheduleController';
 import NotificationController from './app/controllers/NotificationController';
-import Appointment from './app/models/Appointment';
+import AvailableController from './app/controllers/AvailableController';
 
 const routes = new Router();
 const upload = multer(multerConfig);
@@ -26,6 +26,7 @@ routes.put('/users', UserController.update);
 
 // providers
 routes.get('/providers', ProviderController.index);
+routes.get('/providers/:providerId/available', AvailableController.index);
 
 // appointments
 routes.post('/appointments', AppointmentController.store);
